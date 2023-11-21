7a619d414dcd59b7cd137216e56095526d7d3bd7
Ravshan007 <ravshanaliyev9002@gmail.com>
2023-11-21 21:14:58 +0500

    Routes are added successfully

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
+import Layout from "antd/es/layout/layout";
 import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import MainPage from "./components/MainPage";
+import Cryptocurrencies from "./components/Cryptocurrencies";
+import CryptoDetails from "./components/CryptoDetails";
+import Exchanges from "./components/Exchanges";
+import News from "./components/News";
 
 function App() {
   return (
     <div className="app">
-      <Navbar />
+      <div className="navbar">
+        <Navbar />
+      </div>
       <div className="main">
-
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
       </div>
-      <div className="footer">
-
-      </div>
+      <div className="footer"></div>
     </div>
   );
 }
